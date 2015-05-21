e4993d7a104647759b9e2fec18ba7f814103ef82
Jonathan Peypops <jonathanpeypops@msn.com>
2015-05-21 11:40:15 +0200

    matrix

---
+++ src/matrix/Main.java
@@ -0,0 +1,20 @@
+package matrix;
+
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+
+public class Main {
+    public static void main(String[] args) {
+        RealMatrix realMatrixA = MatrixUtils.createRealMatrix(new double[][]{
+                {1,2},
+                {3,4},
+                {5,6}
+        });
+        RealMatrix realMatrixB = MatrixUtils.createRealMatrix(new double[][]{
+                {2,4,6},
+                {1,2,3}
+        });
+        RealMatrix realMatrixC = realMatrixA.multiply(realMatrixB);
+        System.out.println(realMatrixC);
+    }
+}
